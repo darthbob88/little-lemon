7eb20f9b6aa9e9edcf56bf10035fff0e02129d76
Raymond Price <darthbob88@users.noreply.github.com>
2024-08-30 22:03:52 -0700

    Adding state to the booking page.

--- src/BookingPage.js
+++ src/BookingPage.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 function BookingPage() {
     return <div>
         <h2>Book a Table</h2>
@@ -6,11 +8,16 @@ function BookingPage() {
 }
 
 function BookingForm() {
+    const [date, setDate] = useState();
+    const [time, setTime] = useState();
+    const [guests, setGuests] = useState();
+    const [occasion, setOccasion] = useState();
+
     return <form style={{ display: "grid", "maxWidth": "200px", gap: "20px" }}>
         <label htmlFor="res-date">Choose date</label>
-        <input type="date" id="res-date" />
+        <input type="date" id="res-date" value={date} onChange={e => setDate(e.target.value)} />
         <label htmlFor="res-time">Choose time</label>
-        <select id="res-time ">
+        <select id="res-time " value={time} onChange={e => setTime(e.target.value)}>
             <option>17:00</option>
             <option>18:00</option>
             <option>19:00</option>
@@ -19,9 +26,9 @@ function BookingForm() {
             <option>22:00</option>
         </select>
         <label htmlFor="guests">Number of guests</label>
-        <input type="number" placeholder="1" min="1" max="10" id="guests" />
+        <input type="number" placeholder="1" min="1" max="10" id="guests" value={guests} onChange={e => setGuests(e.target.value)} />
         <label htmlFor="occasion">Occasion</label>
-        <select id="occasion">
+        <select id="occasion" value={occasion} onChange={e => setOccasion(e.target.value)} >
             <option>Birthday</option>
             <option>Anniversary</option>
         </select>
