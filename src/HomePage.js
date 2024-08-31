524f9215cf668d5375ff6f4b8691324d6cb8d1b9
Raymond Price <darthbob88@users.noreply.github.com>
2024-08-30 19:10:31 -0700

    Adding the hero section

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,7 +1,21 @@
+import React from "react";
+import restaurant from "./icons_assets/restaurant.jpg"
+
 function HomePage() {
-    return <div>
-        <h2>Home Page stuff</h2>
-    </div>;
+    return <React.Fragment>
+        <div className="hero">
+            <div>
+                <h2>Little Lemon</h2>
+                <h5>Chicago</h5>
+                <div>
+                    Bacon ipsum dolor amet meatloaf pig prosciutto, tri-tip landjaeger sausage shank pancetta. Sirloin jerky tail beef, spare ribs tenderloin strip steak shoulder short ribs pastrami flank chicken cupim hamburger prosciutto. Chislic capicola pork belly prosciutto filet mignon ham hock pork beef ribs tenderloin jowl short ribs shoulder kevin. Sirloin flank alcatra rump.
+                </div>
+                <a href="/booking" className="button">Reserve a Table</a>
+            </div>
+            <img src={restaurant} alt="The restaurant's interior" />
+        </div>
+
+    </React.Fragment>;
 }
 
 export default HomePage;
