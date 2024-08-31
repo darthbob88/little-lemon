d2af95ceae538dbece3c2884f0d5791192c6e242
Raymond Price <darthbob88@users.noreply.github.com>
2024-08-30 22:38:15 -0700

    Adding an API to get available dates

--- src/Main.js
+++ src/Main.js
@@ -3,9 +3,39 @@ import HomePage from "./HomePage";
 import BookingPage from "./BookingPage";
 import { useReducer } from "react";
 function Main() {
-    const defaultTimes = ["17:00", "18:00", "19:00", "20:00", "21:00", "22:00"]
-    const updateTimes = (date) => defaultTimes;
-    const initializeTimes = () => defaultTimes;
+    // Because I don't recall this course ever explaining how to interact with a bare third-party script like this.
+    const seededRandom = function (seed) {
+        var m = 2 ** 35 - 31;
+        var a = 185852;
+        var s = seed % m;
+        return function () {
+            return (s = s * a % m) / m;
+        };
+    }
+
+    const fetchAPI = function (date) {
+        let result = [];
+        let random = seededRandom(date.getDate());
+
+        for (let i = 17; i <= 23; i++) {
+            if (random() < 0.5) {
+                result.push(i + ':00');
+            }
+            if (random() < 0.5) {
+                result.push(i + ':30');
+            }
+        }
+        return result;
+    };
+    const submitAPI = function (formData) {
+        return true;
+    };
+
+    const updateTimes = (availableTimes, date) => {
+        console.log(date)
+        return fetchAPI(new Date(date));
+    };
+    const initializeTimes = () => fetchAPI(new Date());
     const [availableTimes, dispatch] = useReducer(updateTimes, initializeTimes());
 
     return <main>
